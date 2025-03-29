72e16dbd54a6875d15910a11bd71da0d351097b6
vibhorrrr <104832039+vibhorrrr@users.noreply.github.com>
2025-03-29 16:21:08 +0530

    Add src/main/java/com/anganshiksha/repository/UserRepository.java 1743245594494

---
+++ src/main/java/com/anganshiksha/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.anganshiksha.repository;
+
+import com.anganshiksha.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
