63d9f39937c411220d4eef363d58d7a73642e58b
vibhorrrr <104832039+vibhorrrr@users.noreply.github.com>
2025-03-29 16:21:20 +0530

    Add src/main/java/com/anganshiksha/AnganshikshaApplication.java 1743245606945

---
+++ src/main/java/com/anganshiksha/AnganshikshaApplication.java
@@ -0,0 +1,11 @@
+package com.anganshiksha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnganshikshaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AnganshikshaApplication.class, args);
+    }
+}
